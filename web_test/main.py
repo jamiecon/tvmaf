59f7e7085088c2a8110f6cf0190f873427c9b966
jamiecon <jamie@jamiecon.net>
2019-09-20 00:20:04 +0100

    Added basic Algolia search

--- web_test/main.py
+++ web_test/main.py
@@ -1,11 +1,13 @@
 from flask import Flask, render_template, request
 from google.cloud import firestore
+from algoliasearch.search_client import SearchClient
 
 app = Flask(__name__)
 
 db = firestore.Client()
 
-@app.route('/')
+
+@app.route('/', methods=['GET'])
 def index():
     """Home page"""
     home_data = {
@@ -18,21 +20,28 @@ def index():
     return render_template('index.jinja', **home_data)
 
 
-@app.route('/search', methods=['POST'])
+@app.route('/search', methods=['GET'])
 def search():
-    query = request.form['q']
-    output = ''
-    documents = db.collection('title.basics').stream()
-    for doc in documents:
-        title = doc.to_dict()
-        output += '<br>{0}'.format(title['primaryTitle'])
-    return jsonify(output)
+    query = request.args.get('q', '')
+    page_data = {
+        'query': query,
+    }
+
+    client = SearchClient.create('9JZ8KBSETQ', 'ad297cac926002b5dc094ef1be2ec656')
+    index = client.init_index('dev_titles')
+    results = index.search(query)
+    print(results)
+    page_data['results'] = results
+
+    return render_template('search.jinja', **page_data)
+
 
 @app.route('/movie/<title_id>')
-def movie(title_id):
+def title(title_id):
     page_data = {}
 
-    titles = db.collection('title.basics').where('tconst', '==', title_id).limit(1).stream()
+    titles = db.collection('title.basics').where(
+        'tconst', '==', title_id).limit(1).stream()
     for title in titles:
         page_data['title'] = title.to_dict()
         break
@@ -40,6 +49,7 @@ def movie(title_id):
         return render_template('404.jinja')
 
     return render_template('title.jinja', **page_data)
+
 
 if __name__ == '__main__':
     # This is used when running locally only. When deploying to Google App
