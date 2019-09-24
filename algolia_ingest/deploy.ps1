aebb9116c2e7c041edc37a1e6446b52fcb5f7f2d
jamiecon <jamie@jamiecon.net>
2019-09-24 01:47:40 +0100

    Use current environment variables to set Cloud Function environment variables

--- algolia_ingest/deploy.ps1
+++ algolia_ingest/deploy.ps1
@@ -5,6 +5,6 @@ gcloud functions deploy algolia_push `
 --memory 256MB `
 --timeout 60s `
 --project spry-bus-252723 `
---set-env-vars ALGOLIA_APP_ID=9JZ8KBSETQ,ALGOLIA_API_KEY=ad297cac926002b5dc094ef1be2ec656,ALGOLIA_INDEX_NAME=dev_titles,FIRESTORE_PROJECT_ID=spry-bus-252723 `
+--set-env-vars ALGOLIA_APP_ID=$env:ALGOLIA_APP_ID,ALGOLIA_API_KEY=$env:ALGOLIA_API_KEY,ALGOLIA_INDEX_NAME=$env:ALGOLIA_INDEX_NAME,FIRESTORE_PROJECT_ID=$env:FIRESTORE_PROJECT_ID `
 --trigger-event providers/cloud.firestore/eventTypes/document.write `
 --trigger-resource "projects/spry-bus-252723/databases/(default)/documents/title/{id}"
