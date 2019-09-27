10d2452bbdddef1394b9e4ed204ecd5a225459bd
jamiecon <jamie@jamiecon.net>
2019-09-27 01:44:39 +0100

    Use environment variable to specify project when deploying cloud functions

--- algolia_ingest/deploy.ps1
+++ algolia_ingest/deploy.ps1
@@ -4,7 +4,7 @@ gcloud functions deploy algolia_push `
 --region europe-west1 `
 --memory 256MB `
 --timeout 60s `
---project spry-bus-252723 `
+--project $env:FIRESTORE_PROJECT_ID `
 --set-env-vars ALGOLIA_APP_ID=$env:ALGOLIA_APP_ID,ALGOLIA_API_KEY=$env:ALGOLIA_API_KEY,ALGOLIA_INDEX_NAME=$env:ALGOLIA_INDEX_NAME,FIRESTORE_PROJECT_ID=$env:FIRESTORE_PROJECT_ID `
 --trigger-event providers/cloud.firestore/eventTypes/document.write `
 --trigger-resource "projects/spry-bus-252723/databases/(default)/documents/title/{id}"
--- imdb_ingest/deploy.ps1
+++ imdb_ingest/deploy.ps1
@@ -1,1 +1,8 @@
-gcloud functions deploy imdb_ingest --runtime python37 --entry-point cloud_execute --trigger-http --region europe-west1 --memory 512MB --timeout 540s --project spry-bus-252723
+gcloud functions deploy imdb_ingest `
+--runtime python37 `
+--entry-point cloud_execute `
+--trigger-http `
+--region europe-west1 `
+--memory 512MB `
+--timeout 540s `
+--project $env:FIRESTORE_PROJECT_ID
