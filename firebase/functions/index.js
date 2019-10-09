b8e5e128cba61acc1873a376950b1c61c79ec184
jamiecon <jamie@jamiecon.net>
2019-10-09 18:18:50 +0100

    Add Firestore functions for title create, update and delete

--- firebase/functions/index.js
+++ firebase/functions/index.js
@@ -2,61 +2,104 @@ const functions = require('firebase-functions');
 const admin = require('firebase-admin')
 const algoliasearch = require('algoliasearch');
 
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
-// exports.helloWorld = functions.https.onRequest((request, response) => {
-//  response.send("Hello from Firebase!");
-// });
-
 // configure algolia
 const algolia = algoliasearch(
     functions.config().algolia.appid,
     functions.config().algolia.apikey
 );
+const algoliaIndex = algolia.initIndex('dev_titles');
 
 // initialise firebase
 admin.initializeApp();
 const db = admin.firestore();
 
-exports.algolia_triggered_update = functions.firestore.document('title/{id}')
-    .onWrite((change, context) => {
-        console.log(change);
-        console.log(context);
-        console.log(functions.config().algolia.appid);
-        console.log(functions.config().algolia.apikey);
+exports.algolia_item_create = functions.firestore.document('title/{id}')
+    .onCreate((snapshot, context) => {
+        const id = snapshot.id;
+        console.log('Adding search index entry with ID: ' + id);
 
-        return true;
+        db.collection('title').doc(id).get()
+            .then(result => {
+                return algoliaIndex.saveObject({
+                    objectID: result.id, // set the Algolia objectID as the Firebase .key
+                    displayTitle: result.get('display_title'),
+                    displayYear: result.get('year')
+                });
+            })
+            .then(result => {
+                console.log(result);
+                return result;
+            })
+            .catch(error => {
+                console.log(error);
+            })
+
+        return null;
     });
 
-exports.algolia_full_update = functions.https.onRequest((req, resp) => {
-    const algoliaIndex = algolia.initIndex('dev_titles');
+exports.algolia_item_delete = functions.firestore.document('title/{id}')
+    .onDelete((snapshot, context) => {
+        console.log('Deleting search index entry with ID ' + snapshot.id);
+        algoliaIndex.deleteObject(snapshot.id)
+            .then(result => {
+                console.log(result);
+                return result
+            })
+            .catch(error => {
+                console.log(error);
+            })
 
-    const records = [];
-    console.log('Clearing index');
-    algoliaIndex.clearIndex()
-        .then(result => {
-            console.log('Getting titles');
-            return db.collection('title').get()
+        return null;
+    });
+
+exports.algolia_item_update = functions.firestore.document('title/{id}')
+    .onUpdate((change, context) => {
+        const id = change.before.id;
+        console.log('Updating search index entry with ID ' + id);
+        const doc = change.after.data();
+        algoliaIndex.partialUpdateObject({
+            objectID: id,
+            displayTitle: doc.display_title,
+            displayYear: doc.year
         })
-        .then(result => {
-            console.log('Building index');
-            result.forEach((doc) => {
-                const data = {
-                    objectID: doc.id, // set the Algolia objectID as the Firebase .key
-                    displayTitle: doc.get('display_title'),
-                    displayYear: doc.get('year')
-                }
-                records.push(data);
+            .then(result => {
+                console.log(result);
+                return result;
+            })
+            .catch(error => {
+                console.log(error);
+            })
+
+        return null;
+    });
+
+exports.algolia_full_update = functions.https
+    .onRequest((req, resp) => {
+        const records = [];
+        console.log('Clearing index');
+        algoliaIndex.clearIndex()
+            .then(result => {
+                console.log('Getting titles');
+                return db.collection('title').get()
+            })
+            .then(result => {
+                console.log('Building index');
+                result.forEach((doc) => {
+                    const data = {
+                        objectID: doc.id, // set the Algolia objectID as the Firebase .key
+                        displayTitle: doc.get('display_title'),
+                        displayYear: doc.get('year')
+                    }
+                    records.push(data);
+                });
+                return algoliaIndex.saveObjects(records);
+            })
+            .then(result => {
+                console.log(result);
+                resp.send('OK');
+                return result;
+            })
+            .catch(error => {
+                console.log(error);
             });
-            return algoliaIndex.saveObjects(records);
-        })
-        .then(result => {
-            console.log('All done: ' + result);
-            resp.send('OK');
-            return result;
-        })
-        .catch(error => {
-            console.log('Error inserting data: ' + error);
-        });
-});
+    });
